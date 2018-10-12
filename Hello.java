bb037bcac9af27bdeb1119066ae7bd05b8d4c17b
PhamNhatLinh <51600045@student.tdtu.edu.vn>
2018-10-12 12:07:10 +0700

    Hello revision 2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 import java.util.*;
-public class Hello{
+public class Hello1{
 	public static void main(String[] args){
-	   System.out.print("Hello World");
+	   String s="";
+	   Scanner sc=new Scanner(System.in);
+	   System.out.print("Enter Name:");
+	   s=sc.nextLine();
+	   System.out.print("Hello World"+" "+s);
 	}
 }
